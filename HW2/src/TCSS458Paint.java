a5956ba106de9a16189053ea358625adc62b8d5f
Michael Pham N <michael.pham@cardcloset.com>
2020-04-23 17:07:53 -0700

    SolidCube

--- HW2/src/TCSS458Paint.java
+++ HW2/src/TCSS458Paint.java
@@ -432,4 +432,64 @@ public class TCSS458Paint extends JPanel
         frame.setLocationRelativeTo( null );
         frame.setVisible( true );
     }
+    
+    public class Point implements Comparable<Point>{
+    	private double x;
+    	private double y;
+    	private double z;
+    	private double w;
+    	
+    	public Point() {}
+    	
+    	public Point(double x, double y, double z, double w) {
+    		this.x = x;
+    		this.y = y;
+    		this.z = z;
+    		this.w = w;
+    	}
+    	
+    	public double getX() {
+    		return x;
+    	}
+
+    	public void setX(double x) {
+    		this.x = x;
+    	}
+
+    	public double getY() {
+    		return y;
+    	}
+
+    	public void setY(double y) {
+    		this.y = y;
+    	}
+
+    	public double getZ() {
+    		return z;
+    	}
+
+    	public void setZ(double z) {
+    		this.z = z;
+    	}
+
+    	public double getW() {
+    		return w;
+    	}
+
+    	public void setW(double w) {
+    		this.w = w;
+    	}
+
+    	@Override
+    	public String toString() {
+    		return "Point [x=" + this.x + ", y=" + this.y + ", z=" 
+    						+ this.z + ", w=" + this.w + "]";
+    	}
+
+    	@Override
+    	public int compareTo(Point o) {
+    		return (int)(this.x - o.x);
+    	}
+    }
+
 }
